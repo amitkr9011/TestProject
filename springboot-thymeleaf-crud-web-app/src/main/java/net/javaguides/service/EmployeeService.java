06d98e246af056e410b631aa38f5abe3ecbe2b5f
amitk <amitk@amitkr9123>
2021-06-05 12:26:13 +0530

    changes

--- springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeService.java
+++ springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeService.java
@@ -4,7 +4,12 @@ import java.util.List;
 import net.javaguides.model.Employee;
 
 public interface EmployeeService {
+
 	List<Employee> getAllEmployees();
+
 	void saveEmployee(Employee employee);
 
+	Employee getEmployeeById(long id);
+
+	void deleteEmployeeById(long id);
 }
--- springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeServiceImpl.java
+++ springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeServiceImpl.java
@@ -1,6 +1,8 @@
 package net.javaguides.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import net.javaguides.model.Employee;
@@ -21,5 +23,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public void saveEmployee(Employee employee) {
 		// TODO Auto-generated method stub
 		this.employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		if (optional.isPresent()) {
+			employee = optional.get();
+		} else {
+			throw new RuntimeException("Employee not found for id ::" + id);
+		}
+		return employee;
+	}
+
+	@Override
+	public void deleteEmployeeById(long id) {
+		this.employeeRepository.deleteById(id);
 	}
 }
